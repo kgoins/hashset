fb5487f40ee591b6eb7c239d14bcce8d0adf22f4
kgoins <14044530+kgoins@users.noreply.github.com>
2021-02-10 01:23:12 -0500

    added comments to make gofmt happy, fixed bug where a nil initvals var could be passed into ctor, causing crash

--- hashset.go
+++ hashset.go
@@ -4,12 +4,19 @@ import "strings"
 
 var placeholder = struct{}{}
 
+// StrHashset is a hashset of strings
 type StrHashset struct {
 	values map[string]struct{}
 }
 
+// NewStrHashset constructs a StrHashet with
+// optional initial values
 func NewStrHashset(initVals ...string) StrHashset {
-	numVals := len(initVals)
+	numVals := 0
+	if initVals != nil {
+		numVals = len(initVals)
+	}
+
 	set := StrHashset{
 		values: make(map[string]struct{}, len(initVals)),
 	}
@@ -21,30 +28,28 @@ func NewStrHashset(initVals ...string) StrHashset {
 	return set
 }
 
-func (set StrHashset) GetSingleValue() string {
-	retVal := ""
-	for val := range set.values {
-		retVal = val
-		break
-	}
-
-	return retVal
-}
-
+// Add inserts one or many strings into the hashset.
+// Duplicate entries will be automatically ignored.
 func (set *StrHashset) Add(items ...string) {
 	for _, item := range items {
 		set.values[item] = placeholder
 	}
 }
 
+// Size returns the number of values currently
+// held within the hashset
 func (set StrHashset) Size() int {
 	return len(set.values)
 }
 
+// IsEmpty returns true if there are no
+// values in the hashset, false otherwise
 func (set StrHashset) IsEmpty() bool {
 	return len(set.values) == 0
 }
 
+// Values returns a slice of all entries
+// within the hashset
 func (set StrHashset) Values() []string {
 	values := make([]string, set.Size())
 
@@ -57,6 +62,8 @@ func (set StrHashset) Values() []string {
 	return values
 }
 
+// Equals returns true if every entry in two
+// hashsets are the same
 func (set StrHashset) Equals(set2 StrHashset) bool {
 	if set.Size() != set2.Size() {
 		return false
@@ -71,11 +78,15 @@ func (set StrHashset) Equals(set2 StrHashset) bool {
 	return true
 }
 
+// Contains returns true if the set contains
+// the input string
 func (set StrHashset) Contains(val string) bool {
 	_, found := set.values[val]
 	return found
 }
 
+// ContainsSubstr returns true if any entry in the
+// set contains the given string entry
 func (set StrHashset) ContainsSubstr(substr string) bool {
 	for val := range set.values {
 		if strings.Contains(val, substr) {
@@ -98,6 +109,8 @@ func (set StrHashset) ContainsSubstrIgnoreCase(substr string) bool {
 	return false
 }
 
+// Clear removes all entries from the existing hashset.
+// This is a constant time operation.
 func (set *StrHashset) Clear() {
 	set.values = make(map[string]struct{})
 }
