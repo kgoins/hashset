092e5adba20ef7d2611954ca97432d50b9d77a0f
kgoins <14044530+kgoins@users.noreply.github.com>
2021-02-10 02:03:45 -0500

    added ctor tests

--- pkg/hashset_test.go
+++ pkg/hashset_test.go
@@ -7,6 +7,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestNew(t *testing.T) {
+	rq := require.New(t)
+
+	emptySet := hashset.NewStrHashset()
+	rq.NotNil(emptySet)
+	rq.Equal(emptySet.Size(), 0)
+
+	setWithVals := hashset.NewStrHashset("val1", "val2")
+	rq.NotNil(setWithVals)
+	rq.Equal(setWithVals.Size(), 2)
+
+	var nilVals []string
+	nilSet := hashset.NewStrHashset(nilVals...)
+	rq.NotNil(nilSet)
+	rq.Equal(nilSet.Size(), 0)
+}
+
 func TestContains(t *testing.T) {
 	rq := require.New(t)
 	set := hashset.NewStrHashset("val1", "VaR2")
